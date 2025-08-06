a999e641ccc165abb9ce0c08f042816551b0987b
Sai Rishik <153508872+rishik0821@users.noreply.github.com>
2025-08-06 14:43:03 +0530

    Time: 0 ms (100%), Space: 40.2 MB (91.51%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,17 +1,26 @@
 class Solution {
     public int fib(int n) {
         int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return bck(n,dp);
-        
-    }
-    int bck(int n, int[] dp ){
         if(n<=1){
             return n;
         }
-        if(dp[n]!=-1){
-            return dp[n];
+       
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return dp[n]=bck(n-1,dp)+bck(n-2,dp);
+        
+        return dp[n];
+        
     }
+    // int bck(int n, int[] dp ){
+    //     if(n<=1){
+    //         return n;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     return dp[n]=bck(n-1,dp)+bck(n-2,dp);
+    // }
 }
