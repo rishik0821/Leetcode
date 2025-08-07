cc0f1eba3583adcc0e64e2a3101e0ade5997256a
Sai Rishik <153508872+rishik0821@users.noreply.github.com>
2025-08-07 08:29:14 +0530

    Time: 1 ms (5.47%), Space: 41.1 MB (49.47%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,50 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
-        List<Integer> n1 = new ArrayList<>();
-        List<Integer> n2 = new ArrayList<>();
-        if(nums.length==0){
-            return 0;
-        }
-        if(nums.length==1){
-            return nums[0];
-        }
+        if(nums.length==1) return nums[0];
+        int[] dp = new int[nums.length];
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-            if(i!=0){
-                n1.add(nums[i]);
-            }
-            if(i!=nums.length-1){
-                n2.add(nums[i]);
-            }
-
+            if(i!=0) list1.add(nums[i]);
+            if(i!=nums.length-1) list2.add(nums[i]);
         }
-        int x = solve(n1);
-        int y = solve(n2);
-        return Math.max(x,y);
-
+    return Math.max(recursion(list1, dp, nums.length-2), recursion(list2, dp, nums.length-2));
     }
-
-    public int solve(List<Integer> nums){
-        int n = nums.size();
-        if(n==0){
-            return 0;
+    public int recursion(List<Integer> nums, int[] dp, int ind){
+        if(ind==0) return nums.get(0);
+        if(ind==1) return Math.max(nums.get(0), nums.get(1));
+        dp[0] = nums.get(0);
+        dp[1] = Math.max(nums.get(0), nums.get(1));
+        for(int i=2;i<nums.size();i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums.get(i));
         }
-            if(n==1){
-                return nums.get(0);
-            }
-            if(n==2){
-                return Math.max(nums.get(0),nums.get(1));
-            }
-            int dp[]=new int[n];
-            dp[0]=nums.get(0);
-            dp[1]=Math.max(nums.get(0),nums.get(1));
-            for(int i=2;i<n;i++){
-               
-                int take = nums.get(i)+dp[i-2];
-                int non = dp[i-1];
-                dp[i]=Math.max(take,non);
-            }
-        
-        return dp[n-1];
-
+    return dp[ind];
     }
 }
